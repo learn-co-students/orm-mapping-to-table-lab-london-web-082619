7082a120b8c72d571624b28653d0d2689ec5891f
github username <github email address>
2019-09-05 13:05:18 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -15,10 +15,10 @@ class Student
 
   def self.create_table
 
-    
-    SQL = CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
     SQL
-    DB[:conn].execute(SQL)
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
