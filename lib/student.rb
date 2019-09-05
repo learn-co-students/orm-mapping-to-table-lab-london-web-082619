402358c49a30f781cdafde633fb76d31a74d5203
github username <github email address>
2019-09-05 13:01:15 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,60 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor :name, :grade
+  attr_reader :id
+  @@all = [] 
+    
+  #when initialized with a name and a grade 
+  #the name attribute can be accessed 
+  #the grade attribute can be accessed. 
   
-end
+    def initialize ( name, grade, id = nil)
+      @name = name 
+      @id = id 
+      @grade = grade
+    @@all << self 
+    end
+    
+    #Student
+    #creates getter for :id, does not provide setter :id
+    #creates the students table in the database 
+
+    def self.create_table
+      sql =  <<-SQL 
+        CREATE TABLE IF NOT EXISTS students (
+          id INTEGER PRIMARY KEY, 
+          name TEXT, 
+          grade INTEGER
+          )
+          SQL
+      DB[:conn].execute(sql) 
+        end  
+      
+        #drops the students table from the database
+
+      def self.drop_table 
+        
+      sql = "DROP TABLE IF EXISTs students"
+      
+      DB[:conn].execute(sql) 
+
+    end
+      #This is an instance method that saves the attributes describing a 
+      #given student to the students table in our database.
+      def save
+        sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+        SQL
+          
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+
+    def self.create(name:, grade:)
+      student = Student.new(name, grade)
+      student.save
+      student
+    end
+    
+ end
+    
