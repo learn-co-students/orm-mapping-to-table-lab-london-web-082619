024bd6ccea83ecdef116a010ae8ec3b09a8aa500
Keginold <keginoldp@hotmail.com>
2021-05-12 12:58:55 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,46 +1,57 @@
-class Student
-
+class Student 
   attr_accessor :name, :grade
   attr_reader :id 
 
-  def inititalize(name, grade, id=0)
-    @id = id
-    @name = name
+# instance methods
+  def initialize(name, grade, id=nil)
+    @name = name 
     @grade = grade
+    @id = id
   end 
 
-  def self.create_table
+  def save()
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-          name TEXT.
-            grade TEXT
-      )
-      SQL
-    DB[:conn].execute(SQL)
-  end 
-
-  def self.drop_table 
-    sql = <<-SQL
-    DROP TABLE students
+    INSERT INTO students (name, grade) VALUES (?, ?);
     SQL
-  DB[:conn].execute(sql)
-  end 
 
-
-  def save 
-    sql = <<-SQL
-    INSERT INTO students (name, grade)
-    VALUES (?, ?)
-    SQL
     DB[:conn].execute(sql, self.name, self.grade)
 
-    @id - DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students") [0][0]
   end 
 
-  def create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
-  end
+# class methods
+
+def self.create_table()
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+  );
+
+  DB[:conn].execute(sql)
+end
+
+def self.drop_table()
+  sql = <<-SQL
+    DROP TABLE IF EXISTS students 
+    SQL
+
+  DB[:conn].execute(sql)
+end 
+
+def self.create(name:, grade:)
+  new_student = Student.new(name, grade)
+  new_student.save()
+  new_student
+end 
+
+def self.drop_table()
+  sql = <<-SQL
+    DROP TABLE IF EXISTS students
+  SQL
+
+  DB[:conn].execute(sql)
+end
+
 end
